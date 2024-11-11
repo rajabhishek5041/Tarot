21c20892ea3dea36df5a25bf739db3de581f764b
rajabhishek5041 <rajabhishek5041@gmail.com>
2024-11-11 09:48:28 +0530

    TarotCard

---
+++ TarotCard/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-green-800'>Abhishek kumar singh</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
